cfaf8329fe62763eb0186b08be07d4d69a8f6f38
Ying Mao <ying.mao@wemoscooter.com>
2022-09-13 01:59:47 +0800

    test: add test

--- api/order/service_test.go
+++ api/order/service_test.go
@@ -2,12 +2,15 @@ package order
 
 import (
 	"exam/model"
-	"fmt"
 	"sync"
 	"testing"
+	"time"
 )
 
-func TestTradingAndUpdateOrder(t *testing.T) {
+func TestTradingAndUpdateOrderConcurrent(t *testing.T) {
+	model.TradeList = make([]model.Trade, 0)
+	model.Db.OrderMap = make(map[string]model.Order)
+	model.Db.PendingOrderMap = make(map[string]map[string][]model.Order)
 	fakeOrders := []Param{
 		{
 			Type:     0,
@@ -71,7 +74,72 @@ func TestTradingAndUpdateOrder(t *testing.T) {
 	}
 	wg.Wait()
 
-	fmt.Println("===")
+	model.PrintTrade()
+	model.PrintAllOrder()
+}
+
+func TestTradingAndUpdateOrder(t *testing.T) {
+	model.TradeList = make([]model.Trade, 0)
+	model.Db.OrderMap = make(map[string]model.Order)
+	model.Db.PendingOrderMap = make(map[string]map[string][]model.Order)
+	fakeOrders := []Param{
+		{
+			Type:     0,
+			Quantity: 10,
+			Price:    1,
+		},
+		{
+			Type:     0,
+			Quantity: 5,
+			Price:    10,
+		},
+		{
+			Type:     0,
+			Quantity: 2,
+			Price:    1,
+		},
+		{
+			Type:     1,
+			Quantity: 3,
+			Price:    1,
+		},
+		{
+			Type:     1,
+			Quantity: 4,
+			Price:    10,
+		},
+		{
+			Type:     1,
+			Quantity: 3,
+			Price:    10,
+		},
+		{
+			Type:     1,
+			Quantity: 10,
+			Price:    10,
+		},
+		{
+			Type:     1,
+			Quantity: 10,
+			Price:    1,
+		},
+		{
+			Type:     1,
+			Quantity: 2,
+			Price:    1,
+		},
+		{
+			Type:     1,
+			Quantity: 1,
+			Price:    1,
+		},
+	}
+
+	for _, v := range fakeOrders {
+		TradingAndUpdateOrder(v)
+		time.Sleep(1000 * time.Millisecond)
+	}
+
 	model.PrintTrade()
 	model.PrintAllOrder()
 }
